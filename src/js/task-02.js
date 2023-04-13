b74cfaeeafa43359574bb79cd898185e0cccb6a0
OstapinaOtaman <114955051+OstapinaOtaman@users.noreply.github.com>
2023-04-13 22:36:45 +0200

    add first 3 tasks

---
+++ src/js/task-02.js
@@ -0,0 +1,27 @@
+const ingredients = [
+	'Potatoes',
+	'Mushrooms',
+	'Garlic',
+	'Tomatos',
+	'Herbs',
+	'Condiments',
+];
+
+const ingredientsRef = document.querySelector('#ingredients');
+
+const ingredientsArr = ingredients.map((ingredient) => {
+	const listItem = document.createElement('li');
+	listItem.textContent = ingredient;
+	listItem.classList = 'item';
+
+	return listItem;
+});
+
+ingredientsRef.append(...ingredientsArr);
+
+console.log(ingredients)
+
+
+
+
+
